1f4e044ea84469f91d30e108c1d1814fc60eb44e
h0p3hryhoruk <132903002+h0p3hryhoruk@users.noreply.github.com>
2024-02-24 00:40:44 +0200

    Create task4.py
    https://replit.com/@grigoruknadia15/HWtask3-1#main.py

---
+++ prog/l1/task4.py
@@ -0,0 +1,8 @@
+klas1 = int(input("Кількість учнів у першому класі: "))
+klas2 = int(input("Кількість учнів у другому класі: "))
+klas3 = int(input("Кількість учнів у третьому класі: "))
+
+total_students = klas1 + klas2 + klas3 
+total_desks = (total_students + 1)  // 2
+
+print("Потрібно закупити {} парт(и)", total_desks)
